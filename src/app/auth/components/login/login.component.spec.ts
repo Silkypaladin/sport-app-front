34fc48452d6f1f11ec7163a93f8f67f73116b7de
Silkypaladin <bednarski.szymon99@gmail.com>
2022-02-18 16:39:47 +0100

    Clean up tests (for now), fix line endings

--- src/app/auth/components/login/login.component.spec.ts
+++ src/app/auth/components/login/login.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockFormBuilder;
 
   beforeEach(async () => {
+    mockFormBuilder = jasmine.createSpyObj(['group']);
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        { provide: FormBuilder, useValue: mockFormBuilder }
+      ],
     })
     .compileComponents();
   });
@@ -16,10 +22,6 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
