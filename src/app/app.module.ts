72a0c872c4169d2742db617e7816f61ff2df3705
Piotr Rasiński <piotr.rasinski@supermemo.com>
2022-02-25 14:58:44 +0100

    quick state cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { userReducer } from './state/app.reducer';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {EffectsModule} from '@ngrx/effects';
+import {rootReducer} from './state/app.reducer';
 
 @NgModule({
   declarations: [
@@ -16,11 +16,12 @@ import { userReducer } from './state/app.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({currentUser: userReducer}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreModule.forRoot(rootReducer),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/state/app.reducer.ts
+++ src/app/state/app.reducer.ts
@@ -1,23 +1,10 @@
-import { createReducer } from "@ngrx/store";
-import { User } from "src/common/interfaces/User";
-
-
-// Here I'm' declaring required global state slices (like user data or app settings)
-
-interface UserState {
-    userData: User | null,
-    token: string
-};
+import {ActionReducerMap } from "@ngrx/store";
+import * as fromAuth from "../auth/state/auth.reducer";
 
 export interface State {
-    currentUser: UserState
-};
+  auth: fromAuth.AuthState
+}
 
-const initialUserState: UserState = {
-    userData: null,
-    token: ''
-};
-
-export const userReducer = createReducer<UserState>(
-    initialUserState
-);
+export const rootReducer: ActionReducerMap<State> = {
+  auth: fromAuth.authReducer
+}
