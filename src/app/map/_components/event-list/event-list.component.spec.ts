519d4984099689de76835c3a9e6fc746fb840b4f
Silkypaladin <bednarski.szymon99@gmail.com>
2022-02-28 16:56:18 +0100

    Add DI to tests

--- src/app/map/_components/event-list/event-list.component.spec.ts
+++ src/app/map/_components/event-list/event-list.component.spec.ts
@@ -1,20 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventListComponent } from './event-list.component';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 describe('EventListComponent', () => {
   let component: EventListComponent;
   let fixture: ComponentFixture<EventListComponent>;
+  let mockStore: any;
 
   beforeEach(async () => {
+    mockStore = jasmine.createSpyObj(['select']);
     await TestBed.configureTestingModule({
       declarations: [EventListComponent],
+      providers: [{ provide: Store, useValue: mockStore }],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EventListComponent);
     component = fixture.componentInstance;
+    mockStore.select.and.returnValue(of(null));
     fixture.detectChanges();
   });
 
--- src/app/map/_proxy/event-proxy.service.spec.ts
+++ src/app/map/_proxy/event-proxy.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EventProxyService } from './event-proxy.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EventProxyService', () => {
   let service: EventProxyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(EventProxyService);
   });
 
